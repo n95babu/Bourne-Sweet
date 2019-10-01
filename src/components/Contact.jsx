b18ce1d17f38fee13e662a720a45dfef2dd2cdd4
Nazrul Babu <n95babu@gmail.com>
2019-10-01 16:52:18 -0400

    Added contact us Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
 import './Contact.css'
-
-
-
-const Contact = () => {
-
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+const ContactForm = (props) => {
   return (
-    <div className='contact-wrap'>
-      <h1>Contact</h1>
+
+    <div className="form-address" id="contact">
+      <form className="contact-form" onSubmit={props.handleSubmit}><p className="contact-us">Contact Us</p>
+        <input className="input" type="text" name="name" onChange={props.handleChange} placeholder="Name" />
+        <input className="input" type="text" name="Email" onChange={props.handleChange}
+          placeholder="Email" />
+        <input className="input" type="text" name="" onChange={props.handleChange} placeholder="What's on your mind?" />
+        <Button variant="outline-dark">SUMBIT</Button>
+      </form>
+      <div className="address">
+        <p className="bold">Bourne is located in
+            <br />Brooklyn, NY 11238,
+            <br />(718) 000-0000
+            </p></div>
     </div>
-  );
+
+  )
 }
 
-export default Contact;
+export default ContactForm
--- src/components/ControlledCarousel.jsx
+++ src/components/ControlledCarousel.jsx
@@ -10,10 +10,10 @@ class mainCarousel extends Component {
       <div className="mainCarousel">
         <Carousel autoPlay>
           <div>
-            <img src="https://images.unsplash.com/photo-1426869884541-df7117556757?ixlib=rb-1.2.1" />
+            <img src="https://images.unsplash.com/photo-1532943134510-d2ec2a46b561?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
           </div>
           <div>
-            <img src="https://images.unsplash.com/photo-1426869884541-df7117556757?ixlib=rb-1.2.1" />
+            <img src="https://images.unsplash.com/photo-1541618985631-8e9b3b3e73d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
           </div>
           <div>
             <img src="https://images.unsplash.com/photo-1426869884541-df7117556757?ixlib=rb-1.2.1" />
