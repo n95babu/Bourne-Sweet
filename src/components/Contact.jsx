365071c070c552c77d58f923a47e891d4b3d0c17
Nazrul Babu <n95babu@gmail.com>
2019-10-01 17:54:00 -0400

    Added post rquest to from

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,50 @@
-import './Contact.css'
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-const ContactForm = (props) => {
-  return (
+import React from 'react';
+import axios from 'axios';
+import './Contact.css';
+import Button from 'react-bootstrap/Button';
 
-    <div className="form-address" id="contact">
-      <form className="contact-form" onSubmit={props.handleSubmit}><p className="contact-us">Contact Us</p>
-        <input className="input" type="text" name="name" onChange={props.handleChange} placeholder="Name" />
-        <input className="input" type="text" name="Email" onChange={props.handleChange}
-          placeholder="Email" />
-        <input className="input" type="text" name="" onChange={props.handleChange} placeholder="What's on your mind?" />
-        <Button variant="outline-dark">SUMBIT</Button>
-      </form>
-      <div className="address">
-        <p className="bold">Bourne is located in
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+  handleForm = e => {
+    axios.post(
+      "https://formcarry.com/s/SSb8VgBqK1e",
+      this.state,
+      { headers: { "Accept": "application/json" } }
+    )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error)
+      });
+    e.preventDefault();
+  }
+  handleFields = e => this.setState({
+    [e.target.name]: e.target.value
+  })
+
+  render() {
+    return (
+
+      <div className="form-address" id="contact">
+        <form className="contact-form" onSubmit={this.handleForm}><p className="contact-us">Contact Us</p>
+          <input className="input" type="text" name="name" onChange={this.handleFields} placeholder="Name" />
+          <input className="input" type="text" name="Email" onChange={this.handleFields}
+            placeholder="Email" />
+          <input className="input" type="" name="message" onChange={this.handleFields} placeholder="What's on your mind?" />
+          <Button type="submit">SUMBIT</Button>
+        </form>
+        <div className="address">
+          <p className="bold">Bourne is located in
             <br />Brooklyn, NY 11238,
             <br />(718) 000-0000
             </p></div>
-    </div>
+      </div>
 
-  )
+    )
+  }
 }
-
 export default ContactForm
