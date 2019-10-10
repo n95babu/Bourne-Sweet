22ad25a22e722b6ba1a2209b15408acc9a9921f8
Nazrul Babu <n95babu@gmail.com>
2019-10-09 21:53:26 -0400

    Update Pies images

--- src/components/CakePhotos.jsx
+++ src/components/CakePhotos.jsx
@@ -1,46 +1,47 @@
+
 export const photos = [
   {
-    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    src: require('../assets /pie_1.jpg'),
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    src: require('../assets /pie_2.jpg'),
     width: 1,
     height: 1
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    src: require('../assets /pie_3.jpg'),
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    src: require('../assets /pie_4.jpg'),
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    src: require('../assets /pie_5.jpg'),
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    src: require('../assets /pie_6.jpg'),
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    src: require('../assets /pie_7.jpg'),
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    src: require('../assets /pie_8.jpg'),
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    src: require('../assets /pie_9.jpg'),
     width: 4,
     height: 3
   }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,39 +8,37 @@ import logo from '../assets /bsweet.png'
 const Navigation = () => {
   return (
     <>
-      <div className="Nav">
-        <Navbar bg="light" expand="sm" >
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              className='bsweet_logo'
-              alt="Logo"
-            />
-          </Navbar.Brand>
+      <Navbar bg="light" expand="sm" >
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            className='bsweet_logo'
+            alt="Logo"
+          />
+        </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav " />
-          <Navbar.Collapse >
-            <Nav  >
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <NavDropdown title="Sweets" id="basic-nav-dropdown">
-                <Link to='/desserts' >
-                  <NavDropdown.Item href="/desserts">Desserts</NavDropdown.Item>
-                </Link>
-                <Link to='/pies'>
-                  <NavDropdown.Item href="/pies">Pies</NavDropdown.Item>
-                </Link>
-                <Link to='/cakes'>
-                  <NavDropdown.Item href="/cakes">Pies</NavDropdown.Item>
-                </Link>
-                <Link to='/cookies'>
-                  <NavDropdown.Item href="/cookies">Cookies</NavDropdown.Item>
-                </Link>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar >
-      </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav " />
+        <Navbar.Collapse >
+          <Nav  >
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <NavDropdown title="Sweets" id="basic-nav-dropdown">
+              <Link to='/desserts' >
+                <NavDropdown.Item href="/desserts">Desserts</NavDropdown.Item>
+              </Link>
+              <Link to='/pies'>
+                <NavDropdown.Item href="/pies">Pies</NavDropdown.Item>
+              </Link>
+              <Link to='/cakes'>
+                <NavDropdown.Item href="/cakes">Pies</NavDropdown.Item>
+              </Link>
+              <Link to='/cookies'>
+                <NavDropdown.Item href="/cookies">Cookies</NavDropdown.Item>
+              </Link>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar >
     </>
 
   );
