a1a8b86aee6bb95d64a812cb364a7d09710b9b66
Nazrul Babu <n95babu@gmail.com>
2019-10-29 15:04:03 -0400

    Updated mainCarousel images

--- src/components/ControlledCarousel.jsx
+++ src/components/ControlledCarousel.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
-
+// mainCarousel Images
+import car from '../assets /car.png';
+import car2 from '../assets /car2.png';
+import car3 from '../assets /car3.png';
+import car4 from '../assets /car4.png';
 
 class mainCarousel extends Component {
   render() {
@@ -10,16 +14,16 @@ class mainCarousel extends Component {
       <div className="mainCarousel">
         <Carousel autoPlay>
           <div>
-            <img className='img1' src="https://images.unsplash.com/photo-1532943134510-d2ec2a46b561?ixlib=rb-1.2.1" alt="Pic_Image" />
+            <img className='img1' src={car} alt="Pic_Image" />
           </div>
           <div>
-            <img className='img1' src="https://images.unsplash.com/photo-1541618985631-8e9b3b3e73d8?ixlib=rb-1.2.1" alt="Pic_Image" />
+            <img className='img1' src={car2} alt="Pic_Image" />
           </div>
           <div>
-            <img className='img1' src="https://images.unsplash.com/photo-1426869884541-df7117556757?ixlib=rb-1.2.1" alt="Pic_Image" />
+            <img className='img1' src={car3} alt="Pic_Image" />
           </div>
           <div>
-            <img className='img1' src="https://images.unsplash.com/photo-1504674138570-5fe6671ec5ed?ixlib=rb-1.2.1" alt="Pic_Image" />
+            <img className='img1' src={car4} alt="Pic_Image" />
           </div>
         </Carousel>
       </div>
